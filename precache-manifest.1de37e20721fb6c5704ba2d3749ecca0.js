6c0eb211009b1606a87ba7e24cdad3aa335eeb13
Nahuel Federico <alegrenahuel@gmail.com>
2019-12-19 18:04:11 -0300

    Updates

---
+++ precache-manifest.1de37e20721fb6c5704ba2d3749ecca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11fc6395938195a76a6b1646d0d6b31",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "50ba64a82a4d7563b65a",
+    "url": "/tasks/static/css/main.acbc0c45.chunk.css"
+  },
+  {
+    "revision": "fbed7d72acb5945c1be1",
+    "url": "/tasks/static/js/2.801a700c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tasks/static/js/2.801a700c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50ba64a82a4d7563b65a",
+    "url": "/tasks/static/js/main.430645b6.chunk.js"
+  },
+  {
+    "revision": "bb7652f4749fe85d9f2f",
+    "url": "/tasks/static/js/runtime-main.caf33622.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tasks/static/media/logo.5d5d9eef.svg"
+  }
+]);
